5fab398b7e8d6b276535773f61ac430d7a850400
Wade Penistone <wadespackages@yahoo.com>
2017-03-12 15:36:39 +0000

    Added package service provider

---
+++ src/Wadepenistone/Devicecrafting/DevicecraftingServiceProvider.php
@@ -0,0 +1,16 @@
+<?php namespace Wadepenistone\Devicecrafting;
+
+use Illuminate\Support\ServiceProvider;
+
+class DevicecraftingServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        die('Booting new service provider');
+    }
+}
