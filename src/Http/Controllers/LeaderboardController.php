a0bc28b1f14dda8be63ba9e396c46b2351cddee2
Wade Penistone <wadespackages@yahoo.com>
2017-03-18 15:59:51 +0000

    Implemented pagination variables

--- src/Http/Controllers/LeaderboardController.php
+++ src/Http/Controllers/LeaderboardController.php
@@ -7,13 +7,18 @@ class LeaderboardController extends Controller
 {
 	public function index()
     {
+		$battlesPerSection = 5;
+		$playersPerSection = 10;
+
 		// TODO: Replace placeholder data
 		$battles = [
 			0 => [
 
 			]
 		];
-		$players = Robot::all()->take(10);
-        $this->setContent( compact('battles', 'players') );
+		$players = Robot::all()->take($playersPerSection);
+        $this->setContent( compact(
+			'battles', 'players', 'battlesPerSection', 'playersPerSection'
+		));
     }
 }
