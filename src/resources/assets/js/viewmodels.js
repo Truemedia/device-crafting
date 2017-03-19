7ad8a184bf8023a97fd114d1c348cd14f62545f9
Wade Penistone <wadespackages@yahoo.com>
2017-03-19 20:11:11 +0000

    Implemented transition interfaces

--- src/resources/assets/js/viewmodels.js
+++ src/resources/assets/js/viewmodels.js
@@ -1,7 +1,9 @@
 var RobotViewModel = new Vue({
     el: '[data-vm="robot"]',
     data: {
-        robots: []
+        robots: [],
+        robot: {},
+        mode: null
     },
     delimiters: ['${', '}'], // Template literal
     mounted: function() {
@@ -32,11 +34,35 @@ var RobotViewModel = new Vue({
         list: function() {
             fetch('/robot')
                 .then( (res) => { return res.json(); })
-                .then( (json) => { this.robots = json; });
+                .then( (json) => {
+                    this.robots = json;
+                    this.mode = 'list';
+                });
         },
         /**
-          * Import/Export
+          * Interfaces
           */
+        cancel: function() {
+            switch (this.mode) {
+                case 'create':
+                case 'update':
+                    this.mode = 'list';
+                break;
+                default:
+                    this.mode = null;
+                break;
+            }
+        },
+        form: function(id = null) {
+            if (id != null) {
+                this.mode = 'update';
+                let index = this.robots.findIndex( (robot) => robot.id == id );
+                this.robot = this.robots[index];
+            } else {
+                this.mode = 'create';
+                this.robot = [];
+            }
+        },
         importer: function() {
 
         }
